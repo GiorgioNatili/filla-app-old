4fd0aa5b7bfe03c90a6e81535bec4325266127ad
giorgionatili <g.natili@gnstudio.com>
2015-05-09 10:25:52 -0400

    Fix the labels inside the icons

--- SignMyPad/utils/UIButtonExtension.swift
+++ SignMyPad/utils/UIButtonExtension.swift
@@ -19,7 +19,34 @@ extension UIButton {
         let iconImageView = UIImageView(image: icon!)
         iconImageView.frame = CGRect(x: leftMargin, y: y, width: w!, height: h!)
         
+        let currentLabel:String! = titleLabel?.text
+        
+        positionLabelRespectToImage(spacing: 10.0, position: iconImageView.frame)
         addSubview(iconImageView)
+        
+    }
+    
+    private func positionLabelRespectToImage(#spacing: CGFloat, position: CGRect) {
+     
+        let left = position.origin.x + position.width + spacing
+        self.titleEdgeInsets = UIEdgeInsets(top: 0, left: left, bottom: 0, right: 0)
+        
+    }
+    
+    private func updateLabelFrame(view:UIImageView) {
+        
+        let currentFrame = titleLabel?.frame
+        
+        let x = view.frame.origin.x + view.frame.width
+        let y = currentFrame!.origin.y
+        let w = frame.width - view.frame.width
+        let h = currentFrame!.height
+        
+        var titleInsets: UIEdgeInsets = UIEdgeInsets(top: x, left: y, bottom: 0, right: 0)
+        self.titleEdgeInsets = titleInsets
+        //let updatedFrame = CGRect (x: 60, y: y, width: w, height: h)
+        
+        //titleLabel?.frame = updatedFrame
         
     }
     
